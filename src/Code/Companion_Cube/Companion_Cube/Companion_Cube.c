111ba2109f199f8fa0e710f79a4db06ae8ab1dad
Insomniac10102 <ins10102@gmail.com>
2014-12-11 06:55:48 -0800

    Testing out functions
    Code works thus far.

--- src/Code/Companion_Cube/Companion_Cube/Companion_Cube.c
+++ src/Code/Companion_Cube/Companion_Cube/Companion_Cube.c
@@ -1,17 +1,53 @@
-/*
+/**
  * Companion_Cube.c
  *
  * Created: 12/11/2014 5:42:15 AM
- *  Author: TyDesktop
+ * Author: Tyler Hackett
+ *
+ * This project is designed for the purpose of the "Blinky Companion Cube" project, which can be found here: 
+ * https://github.com/Insomniac10102/3D-LED-Companion-Cube
+ *
  */ 
 
+#define PIN_OUT		0x01
+#define PIN_IN		0x00
 
 #include <avr/io.h>
 
+char get_pin(char pin)
+{
+	_NOP(); // First execute a no-operation, as recommended in the datasheet.
+	return PINB >> pin & 0x01;
+}
+
+void set_pin(char pin, char val)
+{
+	PORTB |= (val & 0x01) << pin;
+}
+
+void toggle_pin(char pin)
+{
+	PINB |= 0x01 << pin;
+}
+
+/**
+* Sets the I/O direction of the specified pin. 1 for output, 0 for input.
+*/
+void set_pin_dir(char pin, char dir)
+{
+	DDRB |= (dir & 0x01) << pin;
+}
+
 int main(void)
 {
+	set_pin_dir(0, PIN_IN);  // PB0 is connected to button.
+	set_pin_dir(1, PIN_OUT); // PB1-4 are LEDs.
+	set_pin_dir(2, PIN_OUT);
+	set_pin_dir(3, PIN_OUT);
+	set_pin_dir(4, PIN_OUT);
+	
     while(1)
     {
-        //TODO:: Please write your application code 
+		set_pin(1, 1);
     }
 }
---
+++ src/Code/Companion_Cube/Companion_Cube/Debug/Companion_Cube.d
@@ -0,0 +1,37 @@
+Companion_Cube.d Companion_Cube.o: .././Companion_Cube.c \
+ s:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\io.h \
+ s:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ s:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\inttypes.h \
+ s:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\lib\gcc\avr\4.8.1\include\stdint.h \
+ s:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\stdint.h \
+ s:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\iotn85.h \
+ s:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\iotnx5.h \
+ s:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ s:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\common.h \
+ s:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\version.h \
+ s:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ s:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\lock.h
+
+s:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+s:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+s:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+s:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\lib\gcc\avr\4.8.1\include\stdint.h:
+
+s:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\stdint.h:
+
+s:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\iotn85.h:
+
+s:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\iotnx5.h:
+
+s:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+s:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+s:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+s:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+s:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\lock.h:

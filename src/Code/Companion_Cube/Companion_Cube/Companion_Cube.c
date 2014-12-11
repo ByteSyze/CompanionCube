e4e0dc3f549b1071d14c62ab4c3efc61854a4067
Insomniac10102 <ins10102@gmail.com>
2014-12-11 05:50:29 -0800

    Initial code
    Project is set up in Atmel Studio.

---
+++ src/Code/Companion_Cube/Companion_Cube/Companion_Cube.c
@@ -0,0 +1,17 @@
+/*
+ * Companion_Cube.c
+ *
+ * Created: 12/11/2014 5:42:15 AM
+ *  Author: TyDesktop
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
